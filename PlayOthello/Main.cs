6e98aa786e808ad73e019aba90d8461951404d9c
kowboy702 <mac@macbook-pro-2.attlocal.net>
2015-07-21 22:48:11 -0400

    Visual C# version of othello

---
+++ PlayOthello/Main.cs
@@ -0,0 +1,528 @@
+﻿	/*
+	*
+	*	JENSON JOSEPH
+	*	ARTIFICIAL INTELLIGENCE 
+	*	ORTHELLO PROGRAM
+	*
+	*/
+	
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayOthello
+{
+    public partial class frmMain : Form
+    {
+        private class GridMove:IComparable<GridMove>
+        {
+            public int Quality = 0;
+            public int PosX = 0;
+            public int PoxY = 0;
+            public List<int> Moves;
+
+            public int CompareTo( GridMove g )
+            {
+                return g.Quality - Quality;
+            }
+        }
+
+        public enum GameplayState { gs_default, gs_playing, gs_turnwait, gs_gameend };
+        public enum GameTile { gt_blank, gt_black, gt_white }
+
+        private PictureBox[] Boxes = null;
+        private GameTile[] Grid = null;
+
+        private GameplayState GameState = GameplayState.gs_default;
+        private GameTile CurrentPlay;
+
+        private static int OrthelloSize = 8;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            BuildGameBoxes( this.flpPlayItems );
+        }
+
+        private Image LoadTile(GameTile TileType)
+        {
+            string str = "orthello-0";
+            if (TileType == GameTile.gt_black) str += "2";
+            else if (TileType == GameTile.gt_white) str += "3";
+            str += ".png";
+
+            return new Bitmap( str );
+        }
+
+        public void SetTile( int x, int y, GameTile Tile )
+        {
+            Boxes[IndexPosition(x, y)].Image = LoadTile(Tile);
+            Grid[IndexPosition(x, y)] = Tile;
+        }
+
+        public void BuildGameBoxes(FlowLayoutPanel Layout)
+        {
+            float CWidth = (Layout.Width) / (OrthelloSize + 1),
+                CHeight = (Layout.Height) / (OrthelloSize+1);
+
+            Boxes = new PictureBox[ (OrthelloSize * OrthelloSize) ];
+            Grid = new GameTile[ Boxes.Length ];
+
+            for (int i = 0; i < OrthelloSize; i++)
+            {
+                for (int j = 0; j < OrthelloSize; j++)
+                {
+                    PictureBox NewTile = new PictureBox();
+
+                    NewTile.Width = NewTile.Height = (int)Math.Min( CWidth, CHeight );
+                    NewTile.BackColor = SystemColors.Control;
+                    NewTile.SizeMode = PictureBoxSizeMode.StretchImage;
+                    NewTile.Tag = IndexPosition(j, i);
+                    NewTile.Click += clickTile;
+
+                    Label Loc = new Label();
+                    Loc.Text = (i + 1).ToString() + ", " + (j + 1).ToString();
+                    Loc.BackColor = Color.Transparent;
+                    NewTile.Controls.Add(Loc);
+
+                    Layout.Controls.Add( NewTile );
+                    Boxes[(i*OrthelloSize)+j] = NewTile;
+                }
+            }
+        }
+
+        public void clickTile(Object Sender, EventArgs e)
+        {
+            int tag = (int)((Control)(Sender)).Tag;
+
+            tbxPosX.Text = ((tag % OrthelloSize)+1).ToString();
+            tbxPosY.Text = ((tag / OrthelloSize)+1).ToString();
+        }
+
+        public int IndexPosition(int x, int y)
+        {
+            return (y * OrthelloSize) + x;
+        }
+
+        public void InitializeGame( )
+        {
+            int MidBoxX = (OrthelloSize / 2)-1;
+            int MidBoxY = MidBoxX;
+
+            SetTile(MidBoxX, MidBoxY, GameTile.gt_white);
+            SetTile(MidBoxX+1, MidBoxY, GameTile.gt_black);
+            SetTile(MidBoxX, MidBoxY+1, GameTile.gt_black);
+            SetTile(MidBoxX+1, MidBoxY+1, GameTile.gt_white);
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            switch (this.GameState)
+            {
+                case GameplayState.gs_default:
+                case GameplayState.gs_gameend:
+                    {
+                        DialogResult res = MessageBox.Show("Would you like to play first?",
+                            "Play First",
+                            MessageBoxButtons.YesNo,
+                            MessageBoxIcon.Question);
+
+                        InitializeGame();
+
+                        if (res == DialogResult.Yes)
+                        {
+                            GameState = GameplayState.gs_turnwait;
+                            btnPlay.Text = "Waiting on Input";
+                            CurrentPlay = GameTile.gt_black;
+                        }
+                        else
+                        {
+                            GameState = GameplayState.gs_playing;
+                            btnPlay.Text = "AI Play...";
+                            CurrentPlay = GameTile.gt_white;
+                        }
+
+                        picCurrent.Image = LoadTile(CurrentPlay);
+                    } break;
+                case GameplayState.gs_turnwait:
+                    {
+                        int x = 0, y = 0;
+                        try
+                        {
+                            x = Int32.Parse(tbxPosX.Text) - 1;
+                            y = Int32.Parse(tbxPosY.Text) - 1;
+                        }
+                        catch (Exception)
+                        {
+                        }
+
+                        List<int> Plays = FindPairsThatSurround(x, y, CurrentPlay);
+
+                        if (Plays.Count == 0 
+                            || Grid[ IndexPosition( x, y )] != GameTile.gt_blank )
+                        {
+                            lblDisplay.Text = "Invalid Play";
+
+                            tbxPosX.Clear();
+                            tbxPosY.Clear();
+                        }
+                        else
+                        {
+                            TurnTiles(x, y, Plays, this.CurrentPlay);
+                            CurrentPlay = (CurrentPlay == GameTile.gt_black) ? GameTile.gt_white : GameTile.gt_black;
+
+                            tbxPosX.Clear();
+                            tbxPosY.Clear();
+
+                            btnPlay.Text = "AI Play...";
+
+                            GameState = GameplayState.gs_playing;
+                        }
+                    } break;
+                case GameplayState.gs_playing:
+                    {
+                        List<GridMove> Moves = GenerateMoves(CurrentPlay);
+
+                        if (Moves.Count == 0)
+                        {
+                            lblDisplay.Text = "NO MOVES, PASS";
+                        }
+                        else
+                        {
+                            GridMove cMove = Moves.First();
+
+                            TurnTiles(cMove.PosX, cMove.PoxY, cMove.Moves, this.CurrentPlay);
+                            CurrentPlay = (CurrentPlay == GameTile.gt_black) ? GameTile.gt_white : GameTile.gt_black;
+
+                            lblDisplay.Text = "Move Tile: Y:" + (cMove.PoxY+1).ToString() + ", X:" + (cMove.PosX+1).ToString(); 
+                        }
+                        GameState = GameplayState.gs_turnwait;
+
+                        btnPlay.Text = "Waiting on Input";
+                    } break;
+            }
+
+            picCurrent.Image = LoadTile(CurrentPlay);
+            doScore();
+        }
+
+        private int verifyTile(GameTile Base, GameTile CTile, int Range)
+        {
+            if (CTile == GameTile.gt_blank) return 0;
+            else if (CTile == Base && Math.Abs(Range) > 1) return 1;
+
+            return 2;
+        }
+
+
+        private void doScore()
+        {
+            int WhiteScore = 0, BlackScore = 0;
+
+            foreach (GameTile Tile in Grid)
+                if (Tile == GameTile.gt_black) BlackScore++;
+                else if (Tile == GameTile.gt_white) WhiteScore++;
+
+            lblBlackScore.Text = "Black Score: " + BlackScore.ToString();
+            lblWhiteScore.Text = "White Score: " + WhiteScore.ToString();
+
+            if (WhiteScore + BlackScore == OrthelloSize * OrthelloSize)
+            {
+                GameState = GameplayState.gs_gameend;
+                btnPlay.Text = "Start";
+
+                MessageBox.Show( "Game End: " + ((WhiteScore>BlackScore)?"White":"Black") + " wins.\n\n Game will reset.",
+                    "Game End",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information );
+            }
+        }
+
+        //  Find locations of items that will turn the players tiles
+
+        private List<int> FindPairsThatSurround(int x, int y, GameTile Tile)
+        {
+            List<int> Result = new List<int>();
+
+            Boolean complete = false;
+            int discovered = 0;
+
+            // Rgiht
+
+            for (int i = x + 1; i < OrthelloSize && !complete; i++)
+            {
+                GameTile CTile = Grid[IndexPosition(i, y)];
+
+                if (CTile == GameTile.gt_blank) complete = true;
+                else if (CTile != Tile) discovered++;
+                else if (CTile == Tile && discovered > 0)
+                {
+                    complete = true;
+                    Result.Add(0);
+                    Result.Add(i);
+                    Result.Add(y);
+                }
+                else complete = true;
+            }
+
+            //  Left
+
+            complete = false;
+            discovered = 0;
+            for (int i = x - 1; i >= 0 && !complete; i--)
+            {
+                GameTile CTile = Grid[IndexPosition(i, y)];
+
+                if (CTile == GameTile.gt_blank) complete = true;
+                else if (CTile != Tile) discovered++;
+                else if (CTile == Tile && discovered > 0)
+                {
+                    complete = true;
+                    Result.Add(1);
+                    Result.Add(i);
+                    Result.Add(y);
+                }
+                else complete = true;
+            }
+
+            //  Top
+
+            complete = false;
+            discovered = 0;
+            for (int i = y - 1; i >= 0 && !complete; i--)
+            {
+                GameTile CTile = Grid[IndexPosition(x, i)];
+
+                if (CTile == GameTile.gt_blank) complete = true;
+                else if (CTile != Tile) discovered++;
+                else if (CTile == Tile && discovered > 0)
+                {
+                    complete = true;
+                    Result.Add(2);
+                    Result.Add(x);
+                    Result.Add(i);
+                }
+                else complete = true;
+            }
+
+            //  Bottom
+
+            complete = false;
+            discovered = 0;
+            for (int i = y + 1; i < OrthelloSize && !complete; i++)
+            {
+                GameTile CTile = Grid[IndexPosition(x, i)];
+
+                if (CTile == GameTile.gt_blank) complete = true;
+                else if (CTile != Tile) discovered++;
+                else if (CTile == Tile && discovered > 0)
+                {
+                    complete = true;
+                    Result.Add(3);
+                    Result.Add(x);
+                    Result.Add(i);
+                }
+                else complete = true;
+            }
+
+            //  Bottom Right
+
+            complete = false;
+            discovered = 0;
+            for (int i = x+1, j = y+1; i<OrthelloSize && j<OrthelloSize && !complete; i++,j++)
+            {
+                GameTile CTile = Grid[IndexPosition(i, j)];
+
+                if (CTile == GameTile.gt_blank) complete = true;
+                else if (CTile != Tile) discovered++;
+                else if (CTile == Tile && discovered > 0)
+                {
+                    complete = true;
+                    Result.Add(4);
+                    Result.Add(i);
+                    Result.Add(j);
+                }
+                else complete = true;
+            }
+
+            //  Bottom Left
+
+            complete = false;
+            discovered = 0;
+            for (int i = x - 1, j = y + 1; i >= 0 && j < OrthelloSize && !complete; i--,j++)
+            {
+                GameTile CTile = Grid[IndexPosition(i, j)];
+
+                if (CTile == GameTile.gt_blank) complete = true;
+                else if (CTile != Tile) discovered++;
+                else if (CTile == Tile && discovered > 0)
+                {
+                    complete = true;
+                    Result.Add(5);
+                    Result.Add(i);
+                    Result.Add(j);
+                }
+                else complete = true;
+            }
+
+            //  Top Left
+
+            complete = false;
+            discovered = 0;
+            for (int i = x - 1, j = y - 1; i >= 0 && j >= 0 && !complete; i--,j--)
+            {
+                GameTile CTile = Grid[IndexPosition(i, j)];
+
+                if (CTile == GameTile.gt_blank) complete = true;
+                else if (CTile != Tile) discovered++;
+                else if (CTile == Tile && discovered > 0)
+                {
+                    complete = true;
+                    Result.Add(6);
+                    Result.Add(i);
+                    Result.Add(j);
+                }
+                else complete = true;
+            }
+
+            //  Top Right
+
+            complete = false;
+            discovered = 0;
+            for (int i = x + 1, j = y - 1; i < OrthelloSize && j >= 0 && !complete; i++,j--)
+            {
+                GameTile CTile = Grid[IndexPosition(i, j)];
+
+                if (CTile == GameTile.gt_blank) complete = true;
+                else if (CTile != Tile) discovered++;
+                else if (CTile == Tile && discovered > 0)
+                {
+                    complete = true;
+                    Result.Add(7);
+                    Result.Add(i);
+                    Result.Add(j);
+                }
+                else complete = true;
+            }
+
+            return Result;
+        }
+
+        private void TurnTiles(int x, int y, List<int> Positions, GameTile ChangeTo )
+        {
+            for (int i = 0; i < Positions.Count; i += 3)
+            {
+                switch ( Positions[i] )
+                {
+                    case 0: // Right
+                        {
+                            for (int k = Positions[i + 1]; k > x; k--)
+                                SetTile(k, Positions[i + 2], ChangeTo);
+                        } break;
+                    case 1: // Left
+                        {
+                            for (int k = Positions[i + 1]; k < x; k++)
+                                SetTile(k, Positions[i + 2], ChangeTo);
+                        } break;
+                    case 2: // Top
+                        {
+                            for (int k = Positions[i + 2]; k < y; k++)
+                                SetTile(Positions[i + 1], k, ChangeTo);
+                        } break;
+                    case 3: // Bottom
+                        {
+                            for (int k = Positions[i + 2]; k > y; k--)
+                                SetTile(Positions[i + 1], k, ChangeTo);
+                        } break;
+                    case 4: // Bottom Right
+                        {
+                            for (int k = Positions[i + 1], l = Positions[ i + 2 ]; k > x && l > y; k--, l--)
+                                SetTile(k, l, ChangeTo);
+                        } break;
+                    case 5: // Bottom Left
+                        {
+                            for (int k = Positions[i + 1], l = Positions[i + 2]; k < x && l > y; k++, l--)
+                                SetTile(k, l, ChangeTo);
+                        } break;
+                    case 6: // Top Left
+                        {
+                            for (int k = Positions[i + 1], l = Positions[i + 2]; k < x && l < y; k++, l++)
+                                SetTile(k, l, ChangeTo);
+                        } break;
+                    case 7: // Top Right
+                        {
+                            for (int k = Positions[i + 1], l = Positions[i + 2]; k > x && l < y; k--, l++)
+                                SetTile(k, l, ChangeTo);
+                        } break;
+                }
+            }
+            SetTile(x, y, ChangeTo);
+        }
+
+        private int CountTurns(int x, int y, List<int> Positions )
+        {
+            int Result = 0;
+            for (int i = 0; i < Positions.Count; i += 3)
+            {
+                switch (Positions[i])
+                {
+                    case 0:
+                    case 1: Result += Math.Abs(Positions[i + 1] - x); break;
+                    case 2:
+                    case 3: 
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 7: Result += Math.Abs(Positions[i + 2] - y); break;
+                }
+            }
+
+            return Result;
+        }
+
+        private List<GridMove> GenerateMoves(GameTile Tile)
+        {
+            List<GridMove> Result = new List<GridMove>();
+
+            for (int i = 0; i < OrthelloSize; i++)
+            {
+                for (int j = 0; j < OrthelloSize; j++)
+                {
+                    if (Grid[IndexPosition(j, i)] == GameTile.gt_blank)
+                    {
+                        List<int> Moves = FindPairsThatSurround(j, i, Tile);
+                        int Qual = CountTurns(j, i, Moves);
+
+                        if (Qual > 0) {
+                            GridMove CMove = new GridMove();
+                            CMove.PosX = j;
+                            CMove.PoxY = i;
+                            CMove.Moves = Moves;
+                            CMove.Quality = Qual;
+                        
+                            Result.Add(CMove);
+                        }
+                    }
+                }
+            }
+
+            Result.Sort();
+            return Result;
+        }
+
+        private void tbxPosX_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
+
---
+++ PlayOthello/Main.designer.cs
@@ -0,0 +1,195 @@
+﻿namespace PlayOthello
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlInteraction = new System.Windows.Forms.Panel();
+            this.lblWhiteScore = new System.Windows.Forms.Label();
+            this.lblBlackScore = new System.Windows.Forms.Label();
+            this.picCurrent = new System.Windows.Forms.PictureBox();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.lblPosY = new System.Windows.Forms.Label();
+            this.lblPosX = new System.Windows.Forms.Label();
+            this.lblDetails = new System.Windows.Forms.Label();
+            this.tbxPosY = new System.Windows.Forms.TextBox();
+            this.tbxPosX = new System.Windows.Forms.TextBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.flpPlayItems = new System.Windows.Forms.FlowLayoutPanel();
+            this.pnlInteraction.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCurrent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlInteraction
+            // 
+            this.pnlInteraction.Controls.Add(this.lblWhiteScore);
+            this.pnlInteraction.Controls.Add(this.lblBlackScore);
+            this.pnlInteraction.Controls.Add(this.picCurrent);
+            this.pnlInteraction.Controls.Add(this.lblDisplay);
+            this.pnlInteraction.Controls.Add(this.lblPosY);
+            this.pnlInteraction.Controls.Add(this.lblPosX);
+            this.pnlInteraction.Controls.Add(this.lblDetails);
+            this.pnlInteraction.Controls.Add(this.tbxPosY);
+            this.pnlInteraction.Controls.Add(this.tbxPosX);
+            this.pnlInteraction.Controls.Add(this.btnPlay);
+            this.pnlInteraction.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlInteraction.Location = new System.Drawing.Point(0, 465);
+            this.pnlInteraction.Name = "pnlInteraction";
+            this.pnlInteraction.Size = new System.Drawing.Size(452, 85);
+            this.pnlInteraction.TabIndex = 1;
+            // 
+            // lblWhiteScore
+            // 
+            this.lblWhiteScore.AutoSize = true;
+            this.lblWhiteScore.Location = new System.Drawing.Point(365, 56);
+            this.lblWhiteScore.Name = "lblWhiteScore";
+            this.lblWhiteScore.Size = new System.Drawing.Size(0, 13);
+            this.lblWhiteScore.TabIndex = 10;
+            // 
+            // lblBlackScore
+            // 
+            this.lblBlackScore.AutoSize = true;
+            this.lblBlackScore.Location = new System.Drawing.Point(262, 56);
+            this.lblBlackScore.Name = "lblBlackScore";
+            this.lblBlackScore.Size = new System.Drawing.Size(0, 13);
+            this.lblBlackScore.TabIndex = 9;
+            // 
+            // picCurrent
+            // 
+            this.picCurrent.Location = new System.Drawing.Point(160, 32);
+            this.picCurrent.Name = "picCurrent";
+            this.picCurrent.Size = new System.Drawing.Size(45, 45);
+            this.picCurrent.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCurrent.TabIndex = 8;
+            this.picCurrent.TabStop = false;
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay.Location = new System.Drawing.Point(261, 12);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(133, 24);
+            this.lblDisplay.TabIndex = 7;
+            this.lblDisplay.Text = "Hit Start to Play";
+            // 
+            // lblPosY
+            // 
+            this.lblPosY.AutoSize = true;
+            this.lblPosY.Location = new System.Drawing.Point(21, 63);
+            this.lblPosY.Name = "lblPosY";
+            this.lblPosY.Size = new System.Drawing.Size(15, 13);
+            this.lblPosY.TabIndex = 6;
+            this.lblPosY.Text = "y:";
+            // 
+            // lblPosX
+            // 
+            this.lblPosX.AutoSize = true;
+            this.lblPosX.Location = new System.Drawing.Point(21, 35);
+            this.lblPosX.Name = "lblPosX";
+            this.lblPosX.Size = new System.Drawing.Size(15, 13);
+            this.lblPosX.TabIndex = 5;
+            this.lblPosX.Text = "x:";
+            // 
+            // lblDetails
+            // 
+            this.lblDetails.AutoSize = true;
+            this.lblDetails.Location = new System.Drawing.Point(21, 12);
+            this.lblDetails.Name = "lblDetails";
+            this.lblDetails.Size = new System.Drawing.Size(74, 13);
+            this.lblDetails.TabIndex = 4;
+            this.lblDetails.Text = "Play Location:";
+            // 
+            // tbxPosY
+            // 
+            this.tbxPosY.Location = new System.Drawing.Point(42, 60);
+            this.tbxPosY.Name = "tbxPosY";
+            this.tbxPosY.Size = new System.Drawing.Size(35, 20);
+            this.tbxPosY.TabIndex = 3;
+            // 
+            // tbxPosX
+            // 
+            this.tbxPosX.Location = new System.Drawing.Point(42, 32);
+            this.tbxPosX.Name = "tbxPosX";
+            this.tbxPosX.Size = new System.Drawing.Size(35, 20);
+            this.tbxPosX.TabIndex = 2;
+            this.tbxPosX.TextChanged += new System.EventHandler(this.tbxPosX_TextChanged);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(137, 3);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(103, 23);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Start";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // flpPlayItems
+            // 
+            this.flpPlayItems.BackColor = System.Drawing.SystemColors.GrayText;
+            this.flpPlayItems.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flpPlayItems.Location = new System.Drawing.Point(0, 0);
+            this.flpPlayItems.Name = "flpPlayItems";
+            this.flpPlayItems.Padding = new System.Windows.Forms.Padding(2);
+            this.flpPlayItems.Size = new System.Drawing.Size(452, 459);
+            this.flpPlayItems.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 550);
+            this.Controls.Add(this.flpPlayItems);
+            this.Controls.Add(this.pnlInteraction);
+            this.Name = "frmMain";
+            this.Text = "Jenson\'s Othello";
+            this.pnlInteraction.ResumeLayout(false);
+            this.pnlInteraction.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCurrent)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlInteraction;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Label lblPosY;
+        private System.Windows.Forms.Label lblPosX;
+        private System.Windows.Forms.Label lblDetails;
+        private System.Windows.Forms.TextBox tbxPosY;
+        private System.Windows.Forms.TextBox tbxPosX;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.FlowLayoutPanel flpPlayItems;
+        private System.Windows.Forms.PictureBox picCurrent;
+        private System.Windows.Forms.Label lblWhiteScore;
+        private System.Windows.Forms.Label lblBlackScore;
+
+    }
+}
+
